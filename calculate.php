2fb8f7c07e0c81f43a737f88445347a01c6a2b38
Ariiq F <ariiq.pooka@gmail.com>
2019-01-07 16:07:48 +0700

    Calc awal

--- calculate.php
+++ calculate.php
@@ -1,3 +1,27 @@
 <?php
+    $sql = "SELECT * FROM tutup_bulan";
+    $res = $conn->query($sql);
 
-?>;e
+    if($row = $res->fetch_assoc()) {
+        $date = new DateTime ($row['current']);
+    }
+
+    $monthdate = $date->format('MY');
+    $tablename = "stokbarang_$monthdate";
+
+    $sql = "UPDATE $tablename SET pemasukan = 0";
+    $conn->query($sql);
+    $sql = "UPDATE $tablename SET pengeluaran = 0";
+    $conn->query($sql);
+
+    $sql = "SELECT * FROM $tablename";
+    $res = $conn->query($sql);
+
+    While($row = $res->fetch_assoc()) {
+        $query = "INSERT INTO $newtablename (kodeBarang,namaBarang,satuanBarang,saldoAwal,saldoAkhir) VALUES ('$row[kodeBarang]','$row[namaBarang]','$row[satuanBarang]',$row[saldoAkhir],$row[saldoAkhir])";
+        if(!$conn->query($query)) {
+            echo "Pemindahan Data gagal";
+        }
+    }
+
+?>
