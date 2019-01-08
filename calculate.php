f0535edc7ddcefd5b9615ad0afb73dc7ef11e605
Ariiq F <ariiq.pooka@gmail.com>
2019-01-08 08:49:10 +0700

    Calculate selesai

--- calculate.php
+++ calculate.php
@@ -1,4 +1,8 @@
 <?php
+    require_once 'connect.php';
+
+    $transaction_date = 'JAN2019';
+
     $sql = "SELECT * FROM tutup_bulan";
     $res = $conn->query($sql);
 
@@ -14,14 +18,33 @@
     $sql = "UPDATE $tablename SET pengeluaran = 0";
     $conn->query($sql);
 
-    $sql = "SELECT * FROM $tablename";
+    $sql = "SELECT mb.kodeBarang, mb.namaBarang, mb.satuanBarang, SUM(b.quantity) AS jumlah_beli FROM belihdr JOIN belidtl b ON (belihdr.noTransaksiBeli=b.noTransaksiBeli) JOIN masterbarang mb ON (b.kodeBarang = mb.kodeBarang) WHERE DATE_FORMAT(belihdr.tanggalTransaksiBeli, '%b%Y') = '$transaction_date' GROUP BY mb.kodeBarang";
     $res = $conn->query($sql);
 
     While($row = $res->fetch_assoc()) {
-        $query = "INSERT INTO $newtablename (kodeBarang,namaBarang,satuanBarang,saldoAwal,saldoAkhir) VALUES ('$row[kodeBarang]','$row[namaBarang]','$row[satuanBarang]',$row[saldoAkhir],$row[saldoAkhir])";
+        $query = "INSERT INTO $tablename (kodeBarang,namaBarang,satuanBarang,pemasukan) VALUES ('$row[kodeBarang]','$row[namaBarang]','$row[satuanBarang]',$row[jumlah_beli]) ON DUPLICATE KEY UPDATE pemasukan = pemasukan + $row[jumlah_beli]";
         if(!$conn->query($query)) {
             echo "Pemindahan Data gagal";
         }
+        else {
+            echo "success";
+        }
     }
+
+    $sql = "SELECT mb.kodeBarang, mb.namaBarang, mb.satuanBarang, SUM(j.quantity) AS jumlah_jual FROM jualhdr JOIN jualdtl j ON (jualhdr.noTransaksiJual=j.noTransaksiJual) JOIN masterbarang mb ON (j.kodeBarang = mb.kodeBarang) WHERE DATE_FORMAT(jualhdr.tanggalTransaksiJual, '%b%Y') = '$transaction_date' GROUP BY mb.kodeBarang";
+    $res = $conn->query($sql);
+
+    While($row = $res->fetch_assoc()) {
+        $query = "INSERT INTO $tablename (kodeBarang,namaBarang,satuanBarang,pengeluaran) VALUES ('$row[kodeBarang]','$row[namaBarang]','$row[satuanBarang]',$row[jumlah_jual]) ON DUPLICATE KEY UPDATE pengeluaran = pengeluaran + $row[jumlah_jual]";
+        if(!$conn->query($query)) {
+            echo "Pemindahan Data gagal";
+        }
+        else {
+            echo "success";
+        }
+    }
+
+    $sql = "UPDATE $tablename SET saldoAkhir = saldoAwal + pemasukan - pengeluaran";
+    $conn->query($sql);
 
 ?>
--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ $password = "88888888";
 $dbname = "database_stock";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
